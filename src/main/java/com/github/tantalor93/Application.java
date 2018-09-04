553cacfe4a2c71451a1194a03bd97ff1c406726e
Tantalor93 <Obenky@gmail.com>
2018-09-04 23:52:28 +0200

    add helm

--- src/main/java/com/github/tantalor93/Application.java
+++ src/main/java/com/github/tantalor93/Application.java
@@ -1,7 +1,7 @@
 package com.github.tantalor93;
 
-import com.github.tantalor93.service.EventsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -9,10 +9,9 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 public class Application {
-    @Autowired
-    EventsService eventsService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
+
 }
